1cb855b4b1b2e00bd1ea911b8e5e6a191f5286b5
satofumiya <sato.workfree@gmail.com>
2022-01-24 23:33:13 +0900

    root to sign_in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "home#index"
+  devise_scope :user do
+    root to: 'users/sessions#new'
+end 
+  
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
+  
 end
