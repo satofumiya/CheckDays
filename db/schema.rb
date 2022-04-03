45ea47e561838ba5ae8087a3827224df15e2f6d1
satofumiya <sato.workfree@gmail.com>
2022-04-03 10:19:54 +0900

    delete/functionSNS

--- db/schema.rb
+++ db/schema.rb
@@ -20,13 +20,6 @@ ActiveRecord::Schema.define(version: 2022_02_06_044352) do
     t.boolean "reachgoal", default: false, null: false
   end
 
-  create_table "tweets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.text "text"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
