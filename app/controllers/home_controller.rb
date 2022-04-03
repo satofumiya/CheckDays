567ea421cbfc2f3e4778d0d92b7ca765b7d6e5bb
satofumiya <sato.workfree@gmail.com>
2022-04-03 13:35:36 +0900

    tweet関連のコードの削除

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @tweets = Tweet.where(user_id: current_user.id).order('created_at DESC')
       @goals = Goal.where(user_id: current_user.id).order('created_at DESC')
     else
       redirect_to new_user_session_path
