f6c4d4441860867479644b2bf662a5a82e0fed86
satofumiya <sato.workfree@gmail.com>
2022-06-26 09:45:36 +0900

    todoリストのレスポンシブ対応

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,11 +4,8 @@
       <div class="d-flex flex-column w-75 bg-white align-items-center">
         <v-row class="mt-5 d-flex flex-wrap">
           <v-col cols="12">
-            <span v-if="subGoalLength < 24" class="text-h4"
-              >「{{ subgoal.title }}」</span
-            >
-            <span v-else class="text-h5">「{{ subgoal.title }}」</span>
-            <h1 class="d-flex justify-end font-weight-bold">
+            <span id="largeTitle">「{{ subgoal.title }}」</span>
+            <h1 class="subTitle d-flex justify-end">
               を達成するためにやることを考えましょう。
             </h1>
           </v-col>
@@ -18,14 +15,19 @@
             cols="12"
             class="d-flex flex-column justify-center align-center mt-3"
           >
-            <v-col cols="8" class="d-flex justify-center mt-5">
-              <v-text-field v-model="title" placeholder="やること" solo
+            <v-col cols="8" class="d-flex justify-center mt-md-5">
+              <v-text-field
+                v-model="title"
+                placeholder="やること"
+                solo
+                id="todoTitle"
             /></v-col>
             <v-col cols="8" class="d-flex justify-end" flat>
               <v-btn
                 @click="addTask"
                 color="blue"
                 class="white--text font-weight-black"
+                id="addTodo"
                 rounded
                 >やることを追加する</v-btn
               >
@@ -34,24 +36,24 @@
         </v-container>
         <v-container>
           <v-card class="d-flex" v-for="(task, index) in tasks" :key="task.id">
-            <v-row cols="10" class="d-flex justify-center">
-              <v-col cols="1" class="d-flex align-center justify-center">
-                <v-icon>fas fa-list</v-icon>
-              </v-col>
-              <v-col cols="1" class="d-flex align-center justify-center">
+            <v-row cols="10" class="d-flex justify-center flex-wrap">
+              <v-col lg="1" class="d-flex align-center justify-center">
+                <v-icon class="mb-1 me-2">fas fa-list</v-icon>
                 <v-checkbox
                   @change="taskUpdate(index, task.id, 'done')"
                   v-model="task.done"
+                  id="taskDone"
                 />
               </v-col>
-              <v-col cols="6">
+              <v-col cols="12" sm="8" class="d-flex align-center">
                 <v-text-field
                   full-width
                   v-model="task.title"
                   v-if="!task.done"
                   @change="taskUpdate(index, task.id, 'title')"
+                  class="recordedTodo"
                 ></v-text-field>
-                <p v-else class="my-3">
+                <p v-else class="my-3 taskText">
                   <strike>{{ task.title }}</strike>
                 </p>
               </v-col>
@@ -128,5 +130,13 @@ p {
 }
 #bg {
   background-color: #e6e6e6;
+}
+@media screen and (minwidth: 1024px) {
+  #largeTitle {
+    font-size: 1rem;
+  }
+  #smallTitle {
+    font-size: 1rem;
+  }
 }
 </style>
